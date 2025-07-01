011e338de6535c6356afbc7e3000456ab0de4f72
Nuri Lacka <nurilacka@gmail.com>
2025-07-01 10:57:24 +0200

    feat: implement update management hooks and services for version control

--- src/renderer/services/index.ts
+++ src/renderer/services/index.ts
@@ -2,5 +2,7 @@ import * as settingsServices from './settings.services';
 import * as projectsServices from './projects.service';
 import * as connectorsServices from './connectors.service';
 import * as gitServices from './git.service';
+import * as updateService from './update.service'
 
-export { settingsServices, projectsServices, connectorsServices, gitServices };
+
+export { settingsServices, projectsServices, connectorsServices, gitServices, updateService };
